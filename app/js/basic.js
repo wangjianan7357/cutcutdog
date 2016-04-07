88035eaf7c3eaa89ed12c4d4d5ead1858cf940e4
wangjianan7357 <wangjianan7357@163.com>
2016-04-07 13:29:50 +0800

    app

--- app/js/basic.js
+++ app/js/basic.js
@@ -112,6 +112,34 @@ function getUrlParam(name) {
     }
 }
 
+function appendFile(path) {
+    var img = new Image();
+    img.src = path;
+
+    img.onload = function () {
+        var that = this;
+        //生成比例 
+        var w = that.width,
+        h = that.height,
+        scale = w / h; 
+        //480  你想压缩到多大，改这里
+        w = 480 || w;
+        h = w / scale;
+
+        //生成canvas
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        $(canvas).attr({width : w, height : h});
+        ctx.drawImage(that, 0, 0, w, h);
+
+        var base64 = canvas.toDataURL('image/jpeg', 1 || 0.8);   //1最清晰，越低越模糊。有一点不清楚这里明明设置的是jpeg。弹出 base64 开头的一段 data：image/png;却是png。
+
+        //f1 = base64;   // 把base64数据丢过去，上传要用。
+
+        var pic = document.getElementById("x");    
+        pic.src = base64; //这里丢到img 的 src 里面就能看到效果了
+    };
+}
 
 (function(mui, com) {
     var myStorage = {};
