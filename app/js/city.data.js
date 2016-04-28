c1551bf4d3822b73a71bae79781e562dd052f522
wangjianan7357 <wangjianan7357@163.com>
2016-04-28 14:34:13 +0800

    APP按设计稿出的静态页

---
+++ app/js/city.data.js
@@ -0,0 +1,31 @@
+var cityData = [{
+	value: '810000',
+	text: '香港',
+	children: [{
+		value: "810100",
+		text: "香港岛"
+	}, {
+		value: "810200",
+		text: "九龙"
+	}, {
+		value: "810300",
+		text: "新界"
+	}]
+}, {
+	value: '820000',
+	text: '澳门',
+	children: [{
+		value: "820100",
+		text: "澳门半岛"
+	}, {
+		value: "820200",
+		text: "离岛"
+	}]
+}, {
+	value: '990000',
+	text: '海外',
+	children: [{
+		value: "990100",
+		text: "海外"
+	}]
+}]
