22468860c408a034d102d772dfa8c0183a30d758
wangjianan7357 <wangjianan7357@163.com>
2016-05-22 22:56:35 +0800

    checkout

--- pc/saas/cart.php
+++ pc/saas/cart.php
@@ -3,6 +3,7 @@ require('../include/common.php');
 require('../include/fun_saas.php');
 
 if ($_REQUEST['action'] == 'list') {
+    $total = 0;
     $list = array();
     $where = 'valid = 1';
 
@@ -21,9 +22,11 @@ if ($_REQUEST['action'] == 'list') {
             // 存在数量
             $result['number'] = $request[$result['id']];
             $list[$result['id']] = $result;
+
+            $total += $result['number'] * $result['sale'];
         }
     }
 
-    callback(array('error' => 0, 'list' => $list));
+    callback(array('error' => 0, 'list' => $list, 'total' => sprintf('%.2f', $total)));
 
 }
