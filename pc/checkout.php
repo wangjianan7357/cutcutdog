ed4ee018019ca433d769680d7531f32aa78d74cf
wangjianan7357 <wangjianan7357@163.com>
2016-05-26 23:03:18 +0800

    相册

--- pc/checkout.php
+++ pc/checkout.php
@@ -1,40 +1,31 @@
 <?php
 
-require_once('../include/fun_web.php');
-require_once('../include/cls_paypal.php');
-require_once('../include/common.php');
-require_once('../include/initial.php');
+require_once('include/fun_web.php');
+require_once('include/cls_paypal.php');
+require_once('include/common.php');
+require_once('include/initial.php');
 
-$_POST['sbt_amount'] = sprintf("%01.2f", $_POST['sbt_amount']);
-$_POST['sbt_status'] = 0;
+$_POST['sbt_status'] = 1;
 $_POST['sbt_read'] = 0;
-$_POST['sbt_logistics'] = 0;
-$_POST['sbt_voucher'] = '';
-$_POST['sbt_fields'] = json_encode(array('products' => $goods, 'remark' => $_POST['sbt_remark']));
+$_POST['sbt_fields']['products'] = array();
+
+$account = 0;
+foreach ($_POST['product'] as $key => $val) {
+    $_POST['sbt_fields']['products'][] = $product_list[$key];
+    $account += $product_list[$key]['sale'] * $val;
+}
 
 $submit = array();
-$submit_arr = array('id', 'number', 'mid', 'email', 'gender', 'firstname', 'lastname', 'mobile', 'country', 'city', 'postcode', 'address', 'amount', 'logistics', 'voucher', 'fields', 'status', 'read');
+$submit_arr = array('id', 'number', 'mid', 'email', 'name', 'phone', 'address', 'amount', 'fields', 'status', 'read');
 
 for($i = 0; $i < count($submit_arr); $i++){
     $submit += array($submit_arr[$i] => $_POST['sbt_' . $submit_arr[$i]]);
 }
 
 $my_db->saveRow('order', $submit);
-$cur_order = $my_db->fetchOne('order', array('id' => $_POST['sbt_id']));
-$cur_order['fields'] = json_decode($cur_order['fields'], true);
 
-
-$done = 1;
-
-$done &= $my_db->saveRow('order', array('status' => 1), array('number' => $_POST['number']));
-
-if ($done) {
-    $cur_order = $my_db->fetchOne('order', array('number' => $_POST['number']));
-
-    if ($cur_order['amount'] > 0) {
-        $payment = new Paypal();
-        $payment->getOrderData($cur_order['id']);
-        echo $payment->createForm();
-    }
-
+if ($cur_order['amount'] > 0) {
+    $payment = new Paypal();
+    $payment->getOrderData($cur_order['id']);
+    //echo $payment->createForm();
 }
