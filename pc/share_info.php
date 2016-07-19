5d5450d9b7d642be62e9a5ca0155f9d74e0c9b0e
wangjianan7357 <wangjianan7357@163.com>
2016-07-19 23:20:01 +0800

    share

---
+++ pc/share_info.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+
+<img src="" />
+
+<div>资料库描述</div>
+
+</body>
+</html>
---
+++ pc/share_photo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+
+<img src="" />
+
+<div>图片描述</div>
+
+</body>
+</html>
