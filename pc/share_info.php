f3cd7ea9af60d06f005fd2c4b5b1e3b8efee4385
wangjianan7357 <wangjianan7357@163.com>
2016-07-20 11:02:20 +0800

    修改分享

--- pc/share_info.php
+++ pc/share_info.php
@@ -2,12 +2,24 @@
 <html lang="zh-CN">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style>
+.tu_stlye{ float:left;width:50%;}
+.tu_stlye img{width:100%;}
+.tu_stlye_zi{ float:left; padding-left:10px; width:45%;}
+.tu_stlye_zi div{width:100%; font-size:14px; color:#333; margin-bottom:10px; line-height:25px;}
+</style>
 </head>
 <body>
-
-<img src="" />
-
-<div>资料库描述</div>
-
+<div style="overflow:hidden; border:#CCC solid 1px; padding:10px;">
+<div class="tu_stlye">
+<img src="images/tu_01.jpg" />
+</div>
+<div class="tu_stlye_zi">
+ <div>名稱: 最強寵物店</div>
+ <div>電話: 99909992</div>
+ <div>地址: 旺角勝利道在雄才大略博士五橱柜奇巧苯基棋梆梆枯枯椅有志者事竟成</div>
+ <div>網址: http://www.baidu.com</div>
+</div>
+</div>
 </body>
 </html>
